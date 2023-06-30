5c4b141044f0868b713778bc079f08b4cd19e718
mukharomdev <mukharomdev@gmail.com>
2023-06-30 08:07:51 +0700

    update:views/welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 @section('pageBeranda','page')
 @section('content')
    <div class ="container-fluid">
-   <img src="{{asset('/img/wahyudin-unnes.png')}}" class="image-fluid rounded mx-auto d-block" height="400" width="800">
+   <img src="{{asset('/img/wahyudin-unnes.png')}}"  class="img-responsive" alt="Responsive image"   width="307" height="240"/>
 
    <p class="lead">Selamat datang di situs Alumni SMAN 1 Banjarharjo angkatan 2005,semoga silaturahmi kita tetap terjaga.</p>
     <p class="lead">
